ccb8a0cd51c2579423f79ec5965eb9dcc909f591
iclark23 <112411185+iclark23@users.noreply.github.com>
2022-09-27 21:49:00 -0400

    finished hw3

--- solution-hw3/js/script.js
+++ solution-hw3/js/script.js
@@ -1,3 +1,9 @@
+// initializing variables
+let basePrice = 2.49;
+let glazePrice = 0;
+let packPrice = 1;
+let totalPrice;
+
 // class constructor for bun - this makes an object
 class Bun{
     glazeName; 
@@ -7,12 +13,10 @@ class Bun{
         this.glazeName = bunName;
         this.bunPrice = bunPrice;
     }
+    priceChange() {
+        glazePrice = this.bunPrice
 }
-// made first object using class constructor (new bun object)
-const keep_Original = new Bun("Keep Original", 0 );
-const sugar_Milk = new Bun("Sugar Milk", 0 );
-const vanilla_Milk = new Bun("Vanilla Milk", 0.50 );
-const double_Chocolate = new Bun("Double Chococlate", 1.50 );
+}
 
 class Pack {
     quantity;
@@ -23,32 +27,51 @@ class Pack {
         this.quantityPrice = quantityPrice
     }
 
+    priceChange(){
+        packPrice = this.quantityPrice
+    }
+
 }
-// made second set of objects 
-const onePack = new Pack("1", 1);
-const twoPack = new Pack("3", 3);
-const sixPack = new Pack("6", 5);
-const twelvePack = new Pack("12", 10);
 
-// made an array of glaze objects and pack objects
-let bunGlazes =[keep_Original, sugar_Milk, vanilla_Milk, double_Chocolate];
-let packs = [onePack, twoPack, sixPack, twelvePack];
+// making an array of glaze objects and pack objects
+let bunGlazes =[new Bun("Keep Original", 0), new Bun("Sugar Milk", 0 ), new Bun("Vanilla Milk", 0.50 ), new Bun("Double Chococlate", 1.50 )];
+let packs = [new Pack("1", 1), new Pack ("3", 3), new Pack ("6", 5) , new Pack ("12", 10) ];
 
-// console.log("hi");
 
 let glazedropdown = document.getElementById("dropdownglaze")
 let packdropdown = document.getElementById("dropdownpacks")
 for (let i = 0; i < bunGlazes.length;i++) {
     // glaze dropdown
     let glazedropdownoption= document.createElement("option")
-    glazedropdownoption.textContent = bunGlazes[i].glazeName
+    glazedropdownoption.innerHTML = bunGlazes[i].glazeName
     glazedropdown.appendChild(glazedropdownoption)
-    // console.log( "I am on bun:");
-    // console.log(bunGlazes[i].glazeName);
 
     //pack dropdown
     let dropdownpacks = document.createElement("option") // makes dropdown option with no names yet
-    dropdownpacks.textContent = packs[i].quantity // gives a dropdown option a name
+    dropdownpacks.innerHTML = packs[i].quantity // gives a dropdown option a name
     packdropdown.appendChild(dropdownpacks)  // adds the options inside the dropdown
+
 }
 
+
+function changeGlazing(element){
+
+    const priceChange = element.value;
+    
+
+    for (i = 0; i < bunGlazes.length; i++){
+        if (bunGlazes[i].glazeName == element.value) {
+            bunGlazes[i].priceChange()
+        }
+
+    }
+
+    for (i = 0; i < packs.length; i++){
+        if (packs[i].quantity == element.value) {
+            packs[i].priceChange()
+        }
+    }
+
+    totalPrice = ((basePrice + glazePrice) * packPrice).toFixed(2);
+    document.getElementById("changePrice").innerHTML = "$" + totalPrice;
+}
